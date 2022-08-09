49f24f56a70f71980658389b575205ab6dfee264
Asztalos Gergő <gergo0317@gmail.com>
2022-08-09 15:42:42 +0200

    added custom_elements_schema

--- projects/ngx-cva-base/src/lib/widgets/widgets.module.ts
+++ projects/ngx-cva-base/src/lib/widgets/widgets.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {BasicInputComponent} from "./basic-input/basic-input.component";
 import {NumberInputComponent} from "./number-input/number-input.component";
@@ -31,7 +31,8 @@ import {MatInputModule} from "@angular/material/input";
     ],
     exports: [
         BasicInputComponent,
-    ]
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class WidgetsModule {
 }
